a0fc7bb9294637f3c6090400f900a4d0f24d15fa
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-02 17:36:47 -0300

    exemplo 1

---
+++ pasta_001/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+   <div id="app">
+       {{estados}}
+       <button @click="usar">alterar</button>
+   </div>
+</template>
+
+<script>
+export default {
+	name: 'app',
+	data() {
+		return {
+			estados : ['sp', 'rj', 'mg', 'sc', 'rs']
+		}
+	},
+	methods: {
+		usar() {
+			this.estados = 'mudou'
+		}
+	}
+}
+</script>
+
+<style>
+    
+</style>
