80e81bd457b7fa9e80521901adf44edbe41bd449
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-02 18:10:13 -0300

    (+++++)

---
+++ teste/README.md
@@ -0,0 +1,5 @@
+# Vue Calculator
+
+Simple calculator made with Vue.js
+
+### [Click here for a live preview](https://kylbutlr-calculator.herokuapp.com/)
---
+++ teste/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #111;
+  margin: 20% auto;
+}
+
+@media only screen and (min-width: 768px) {
+  #app {
+    margin: 5% auto;
+  }
+}
+</style>
