28726fd040ef64062fb8422e2145156878f9538b
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-04 12:31:14 -0300

    exemplo 5

--- pasta_001/src/App.vue
+++ pasta_001/src/App.vue
@@ -1,7 +1,7 @@
 <template>
    <div id="app">
-       <inicio :cores="[...cores]" />
-   </div>
+       <inicio :cores="[...cores]" :cores2="[...cores2]" />
+	</div>
 </template>
 
 <script>
@@ -18,6 +18,10 @@ export default {
 				{id: 3, cor: 'branco', significado: 'paz'},
 				{id: 4, cor: 'amarelo', significado: 'riqueza'},
 				{id: 5, cor: 'azul', significado: 'infinito'},
+			],
+			cores2: [
+				{id: 6, cor: 'laranja', significado: 'alegria'},
+				{id: 7, cor: 'marrom', significado: 'idade antiga'}
 			]
 		}
 	}
--- pasta_001/src/components/Inicio.vue
+++ pasta_001/src/components/Inicio.vue
@@ -3,19 +3,22 @@
            <div v-for="cor in cores" :key="cor.id">
               {{cor.id}}: {{cor.cor}} - {{cor.significado}} 
            </div>
+           <button @click="mudar">mudar</button>
      </div>
 </template>
 
 <script>
 export default {
-    props: {cores: Array},
+    props: {cores: Array, cores2: Array}, 
     data() {
         return {
             cor: null
         }
     },
     methods: {
-       
+       mudar() {
+           this.cores = this.cores2
+       }
     }
 }
 </script>
